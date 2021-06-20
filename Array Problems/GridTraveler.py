12cf5a79064c3c5b5b9117d530b00d6b1789d6ca
Saikiran <saikiranmadala10@gmail.com>
2021-06-21 01:07:41 +0530

    Done

--- Array Problems/GridTraveler.py
+++ Array Problems/GridTraveler.py
@@ -1,1 +1,19 @@
-	
+# m,n ---Dimension of the gridTravler
+def grid_traveller(m, n):
+    memo = {"1,1": 1}
+
+    def helper(m, n):
+        key = f"{m},{n}"
+        if m == 0 or n == 0:
+            return 0
+        if key in memo:
+            return memo[key]
+        if key in memo:
+            return memo[key]
+        memo[key] = helper(m - 1, n) + helper(m, n - 1)
+        return memo[key]
+
+    return helper(m, n)
+
+
+print(grid_traveller(50,10))
