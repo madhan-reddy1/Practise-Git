946fc6fa5a4c1b3c58b70a3a6478b330a01874a2
Sai Kiran <saikiranmadala10@gmail.com>
2022-07-18 23:13:53 -0700

    Added Code Insert Array

---
+++ Recursion/insert_Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void insert(int arr[],int n,int pos,int ele,int capacity){
+
+}
+int main(int argc, char const *argv[])
+{
+	int arr[20]={1,2,3,4,6,7,8}
+	int ele=5;
+	int capacity=20;
+	int pos=5;
+	cout<<"Before Insertion \n";
+    for(int i=0; i < n; i++)
+    {
+       cout<<arr[i];
+    }
+	insert(arr,)	
+	return 0;
+}
